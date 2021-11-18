50e780e7d06d6b59690b4c93ab50eccb944b06d7
Rafael Hiroshi <rafaelhkp@gmail.com>
2021-11-18 15:29:22 -0300

    Set active profile as test profile

--- src/test/java/br/com/alura/forum/ForumApplicationTests.java
+++ src/test/java/br/com/alura/forum/ForumApplicationTests.java
@@ -3,8 +3,10 @@ package br.com.alura.forum;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ForumApplicationTests {
 
 	@Test
